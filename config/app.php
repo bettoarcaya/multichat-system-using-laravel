df1e91f60707703cf34bb1bcec186b5f5e57aba6
Humberto Arcaya <betoarcaya@gmail.com>
2019-12-09 15:01:12 -0400

    ultimos cambios

--- config/app.php
+++ config/app.php
@@ -170,9 +170,9 @@ return [
 
 
          //Barryvdh\Debugbar\ServiceProvider::class,
-         Collective\Html\HtmlServiceProvider::class,
+         //Collective\Html\HtmlServiceProvider::class,
          //Cviebrock\EloquentSluggable\ServiceProvider::class,
-         Unisharp\Ckeditor\ServiceProvider::class,
+         //Unisharp\Ckeditor\ServiceProvider::class,
          Laracasts\Flash\FlashServiceProvider::class,
 
         //
@@ -233,7 +233,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Form' => Collective\Html\FormFacade::class,
       'Html' => Collective\Html\HtmlFacade::class,
 
     ],
