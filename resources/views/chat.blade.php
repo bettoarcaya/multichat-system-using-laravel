f3f48323779c59f639a1a1bc1fb2f83ad81fb325
Humberto Arcaya <betoarcaya@gmail.com>
2019-12-10 13:37:38 -0400

    estructura basica para la seccion de chat

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -1,18 +1,37 @@
-@extends('plantilla.base')
-
-@section('title', 'chat')
-
-
 @extends('layouts.app')
-@section('style')
-  {!! Html::style("css/estilos.css") !!}
+
 @section('content')
 
-  @include('parciales.navbarLogin')
+  <div class="container">
+    <div class="row no-gutters contact-msg-box">
+      {{-- contact content --}}
+      <div class="col-md-4">
+        <div class="row">
+          <div class="col-md-3">
+            <img class="round-img img-fluid" src="{{asset('assets/images/avatar-m.jpeg')}}" alt="">
+          </div>
+          <div class="col-md-9">
+            <p><strong>Nombre contacto</strong></p>
+            <span>Mensja de contacto</span>
+            <p>estado</p>
+          </div>
+        </div>
 
-  <!--<div class="">
-    <div class="">-->
-      <div class="col-sm-10 col-sm-offset-1" id="contMsjs"></div>
+      </div>
+      <div class="col-md-8 left-border">
+        <div class="msg-box">
+          {{-- msj content --}}
+
+        </div>
+        <div>
+            <textarea class="form-control" id="txtMensaje"></textarea>
+            <button type="button" class="btn btn-raised btn-primary" id="btnMensaje">Enviar</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+    {{--<div class="col-sm-10 col-sm-offset-1" id="contMsjs"></div>
      <div class="well">
        <div class="row">
 		     <div class="col-sm-6 col col-sm-offset-1">
@@ -21,13 +40,6 @@
 			   <div class="col-sm-2">
 				    <button type="button" class="btn btn-raised btn-primary" id="btnMensaje">Enviar</button>
 			   </div>
-	      </div>
-      </div>
-    <!--</div>
-  </div>-->
-
-  @include('parciales.footer')
-
-  {!! Html::script('js/jquery-2.1.1.js') !!}
-  {!! Html::script('js/script.js') !!}
+	    </div>
+    </div>--}}
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,25 +6,6 @@
 			<img class="img-fluid" src="https://www.cometchat.com/public/img/cometchat-homepage.png?v=261216" alt="...">
 		</div>
 	</div>
-	{{--<div class="container">
-		<div class="row justify-content-center">
-			<div class="col-md-8">
-				<div class="card">
-					<div class="card-header">Dashboard</div>
-
-					<div class="card-body">
-						@if (session('status'))
-								<div class="alert alert-success" role="alert">
-										{{ session('status') }}
-								</div>
-						@endif
-
-						You are logged in!
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>--}}
 
 	<div class="between-section container">
 		<div class="text-center">
